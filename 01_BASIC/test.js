5ff1d2e63108cf6b105ae5b6ab2d1dd21fc707b5
Shivakumara Swamy P <117889446+ShivuDev@users.noreply.github.com>
2025-05-18 12:31:14 +0000

    tested convertion Operattion

--- 01_BASIC/test.js
+++ 01_BASIC/test.js
@@ -1,3 +1,10 @@
 console.log("Hi Shivu")
 console.log("I'm Back!!")
 console.log("Testing the Environment")
+let score=33
+let scoreInString=String(score)
+let isLoggedIn=true
+let isLoggedIninBool=Boolean(isLoggedIn)
+let name=""
+let nameInBool=Boolean(name)
+console.table([score,typeof score,scoreInString,typeof scoreInString,isLoggedIn,typeof scoreInString,name,typeof name,nameInBool,typeof nameInBool])
