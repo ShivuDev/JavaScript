b9dfdd699e4ec96e7c4151d46d830c819ba39c51
Shivakumara Swamy P <117889446+ShivuDev@users.noreply.github.com>
2025-05-19 17:58:24 +0000

    trial

--- 01_BASIC/test.js
+++ 01_BASIC/test.js
@@ -1,6 +1,15 @@
 console.log("Hi Shivu")
 console.log("I'm Back!!")
 console.log("Testing the Environment")
+<<<<<<< HEAD
+let score=33
+let scoreInString=String(score)
+let isLoggedIn=true
+let isLoggedIninBool=Boolean(isLoggedIn)
+let name=""//empty
+let nameInBool=Boolean(name)//it will be converted into false
+console.table([score,typeof score,scoreInString,typeof scoreInString,isLoggedIn,typeof scoreInString,name,typeof name,nameInBool,typeof nameInBool])
+=======
 let score=33
 let scoreInString=String(score)
 let isLoggedIn=true
