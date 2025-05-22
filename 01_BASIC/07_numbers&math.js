456bdfb1db2aa775e3b7719c36445acdc90a2d4e
Shivakumara Swamy P <117889446+ShivuDev@users.noreply.github.com>
2025-05-22 16:54:53 +0000

    Learnt about number and Math

---
+++ 01_BASIC/07_numbers&math.js
@@ -0,0 +1,24 @@
+
+// const score=new Number(11)
+// console.log(score);
+// const salary = new Number(1000000)
+// console.log(salary.toLocaleString('en-IN'));
+// console.log(score.toFixed(2));
+// console.log(score.toPrecision(3));
+// +++++ Maths
+
+// console.log(Math.abs(-11))
+// console.log(Math.pow(11,2));
+// console.log(Math.sqrt(121))
+// console.log(Math.floor(12.44))
+// console.log(Math.ceil(12.44))
+// console.log(Math.round(12.3));
+
+// Program to get any number between 1 to 9
+
+console.log(Math.round(((Math.random()*10)+1)));
+
+const min=11
+const max=15
+
+console.log(Math.floor(Math.random()*(max - min + 1))+  min);
